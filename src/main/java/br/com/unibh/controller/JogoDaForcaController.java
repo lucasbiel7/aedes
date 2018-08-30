4bafe8c0d1e616fbd1df0c7c55e27b90d0202b43
Lucas Gabriel de Souza Dutra - 11724814 <11724814@animaedu.intranet>
2018-08-29 22:22:27 -0300

    Adicionando Engine para desenhar boneco com as penas e braços sem angulos

--- src/main/java/br/com/unibh/controller/JogoDaForcaController.java
+++ src/main/java/br/com/unibh/controller/JogoDaForcaController.java
@@ -7,7 +7,13 @@ package br.com.unibh.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.ArcType;
 
 /**
  * FXML Controller class
@@ -16,12 +22,87 @@ import javafx.fxml.Initializable;
  */
 public class JogoDaForcaController implements Initializable {
 
+    @FXML
+    private AnchorPane apPrincipal;
+    @FXML
+    private Canvas cForca;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }
 
+    @FXML
+    public void onTentativa(ActionEvent e) {
+        desenhar(6);
+    }
+
+    public static final int INICIO_X = 15;
+    public static final int INICIO_Y = 15;
+
+    public void desenhar(int falhas) {
+        GraphicsContext graphicsContext = cForca.getGraphicsContext2D();
+        switch (falhas) {
+            case 1:
+                desenharCabeca(graphicsContext);
+                break;
+            case 2:
+                desenharCabeca(graphicsContext);
+                desenharCorpo(graphicsContext);
+                break;
+            case 3:
+                desenharCabeca(graphicsContext);
+                desenharCorpo(graphicsContext);
+                desenharBracoDireito(graphicsContext);
+            case 4:
+                desenharCabeca(graphicsContext);
+                desenharCorpo(graphicsContext);
+                desenharBracoDireito(graphicsContext);
+                desenharBracoEsquerdo(graphicsContext);
+                break;
+            case 5:
+                desenharCabeca(graphicsContext);
+                desenharCorpo(graphicsContext);
+                desenharBracoDireito(graphicsContext);
+                desenharBracoEsquerdo(graphicsContext);
+                desenharPernaDireita(graphicsContext);
+                break;
+            case 6:
+                desenharCabeca(graphicsContext);
+                desenharCorpo(graphicsContext);
+                desenharBracoDireito(graphicsContext);
+                desenharBracoEsquerdo(graphicsContext);
+                desenharPernaDireita(graphicsContext);
+                desenharPernaEsquerda(graphicsContext);
+                break;
+        }
+
+    }
+
+    private void desenharCabeca(GraphicsContext graphicsContext) {
+        graphicsContext.fillOval(INICIO_X, INICIO_Y, 50, 50);
+    }
+
+    private void desenharCorpo(GraphicsContext graphicsContext) {
+        graphicsContext.fillRect(INICIO_X + (50 / 2), INICIO_Y + 50, 3, 80);
+    }
+
+    private void desenharBracoDireito(GraphicsContext graphicsContext) {
+        graphicsContext.fillRect(INICIO_X + (50 / 2), INICIO_Y + 65, 50, 3);
+    }
+
+    private void desenharBracoEsquerdo(GraphicsContext graphicsContext) {
+        graphicsContext.fillRect(INICIO_X + (50 / 2) - 50, INICIO_Y + 65, 50, 3);
+    }
+
+    private void desenharPernaDireita(GraphicsContext graphicsContext) {
+        graphicsContext.fillRect(INICIO_X + (50 / 2), INICIO_Y + 50 + 80, 50, 3);
+    }
+
+    private void desenharPernaEsquerda(GraphicsContext graphicsContext) {
+        graphicsContext.fillRect(INICIO_X + (50 / 2) - 50, INICIO_Y + 50 + 80, 50, 3);
+    }
 }
---
+++ src/main/java/br/com/unibh/model/Forca.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.unibh.model;
+
+/**
+ *
+ * @author 11724814
+ */
+public class Forca {
+    
+    private String palavra;
+    private int tentativas;
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public int getTentativas() {
+        return tentativas;
+    }
+
+    public void setTentativas(int tentativas) {
+        this.tentativas = tentativas;
+    }
+    
+    
+}
